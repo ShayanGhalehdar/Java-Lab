3a43894a984f5090697d519503f5cf63fc36428e
Shayan Ghalehdar <100853204+ShayanGhalehdar@users.noreply.github.com>
2023-01-04 14:14:16 +0330

    Delete Lab1(second version)/lab1code directory

--- Lab1(second version)/lab1code/Lab1Q3.java
+++
@@ -1,9 +0,0 @@
-package lab1code;
-
-public class Lab1Q3 
-{
-  public static void main(String[] args) //main method
-    {
-        System.out.println("Hello, World!"); //prinintg a string of letters
-    }  
-}
