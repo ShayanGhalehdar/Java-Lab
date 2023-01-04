38b8d7b852fff7fd6b3acad5fc4932d05a88446f
Shayan Ghalehdar <100853204+ShayanGhalehdar@users.noreply.github.com>
2023-01-04 13:43:02 +0330

    Finalizing Lab 3
    writing the methods like getAncestor and getPath an… …d also writing a test topology for the program

---
+++ Lab3/src/DirectedEdge.java
@@ -0,0 +1,36 @@
+
+
+public class DirectedEdge extends Edge{
+
+    Node inNode;
+    Node outNode;
+
+    String name;
+
+    public DirectedEdge(String name){  // normal constructor
+        this.name = name;
+    }
+
+    public DirectedEdge(String name, Node inNode, Node outNode){ // constructor with initialization
+
+        this.name = name;
+        this.inNode = inNode;
+        this.outNode = outNode;
+
+    }
+
+    public Node[] getter(){ // get the nodes
+        return new Node[]{inNode, outNode};
+    }
+
+    public void setter(Node inNode, Node outNode){  // set the nodes if not set with constructor
+
+        this.inNode = inNode;
+        this.outNode = outNode;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
---
+++ Lab3/src/Edge.java
@@ -0,0 +1,6 @@
+
+public abstract class Edge {
+
+    public abstract Node[] getter();
+    public abstract String getName();
+}
